21f6cee5a8edf923db9bcd01e500723e213fc2d0
Joseph Francioso <joefrancioso@gmail.com>
2023-03-24 13:37:23 -0400

    Submitting ifAnswerIsCorrectFunction

--- exercise/js/app.js
+++ exercise/js/app.js
@@ -1,4 +1,4 @@
-const maxNumberVariableCanBe = 10;  //is inclusive, so this creates 9 
+const maxNumberVariableCanBe = 10; //is inclusive, so this creates 9 
 const maxNumberAnswerCanBe = maxNumberVariableCanBe * maxNumberVariableCanBe;
 
 
@@ -10,13 +10,34 @@ const currentScore = document.querySelector('.currentScore');
 const answersList = document.querySelectorAll('.li');
 const startOverButton = document.querySelector('.btnStartOver');
 const summaryScreen = document.querySelectorAll('.show-hide');
-
+const mainElement = document.querySelector('.main');
 
 //initialize the score/problemSet/currentProblemIndex/generates 10 problems
 let score = 0;
 let problemSet = generateProblemSet(10);
 let currentProblemIndex = 0;
 
+//add an event listener to update the score if the answer is correct
+
+answersList.foreach((answer, index) => {
+    answer.addEventListener('click', () => {
+        const isCorrect = checkAnswer(problemSet[currentProblemIndex], index)
+        if (isCorrect) {
+            score++;
+            currentScore.textContent = score;
+        }
+        currentProblemIndex++;
+        if (currentProblemIndex < problemSet.Length) {
+            displayProblem[problemSet[currentProblemIndex]]
+        } else {
+            summaryScreen.forEach(problemSet => {
+                //lement.remove('hidden');
+            });
+
+
+        }
+    });
+})
 
 
 getRandomNumber();
@@ -25,23 +46,24 @@ getRandomNumber();
  * Utility function to generate a random number based on max
  * maxNumber declared at top (currently set to 10 so range is 0-9)
  */
- function getRandomNumber(max) {
+function getRandomNumber(max) {
     return Math.floor(Math.random() * Math.floor(max));
-  }
+}
 
-  /**
+/**
  * Utility function to shuffle the items in an array
  * @param {object} arr
  */
 function shuffleArray(arr) {
-    return arr.sort(function (a, b) { return Math.random() - 0.5 })
-  }
+    return arr.sort(function(a, b) { return Math.random() - 0.5 })
+}
 
 
 
-  document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     //FUNCTION CALLS
 
 
+
     //EVENT LISTENERS 
-  });
+});
